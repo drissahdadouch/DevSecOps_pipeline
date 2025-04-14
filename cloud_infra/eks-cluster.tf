82985959214bfb479c81364b31ec5c982faea2a5
drissahdadouch <drissahd@gmail.com>
2025-04-14 19:16:23 +0100

    add the new user

--- cloud_infra/eks-cluster.tf
+++ cloud_infra/eks-cluster.tf
@@ -20,7 +20,11 @@ resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
 resource "aws_eks_cluster" "myapp_eks_cluster" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_role.arn
-  version  = "1.28" # Optional but recommended
+  version  = "1.28"
+  access_config {
+    authentication_mode                         = "API"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
 
   vpc_config {
     subnet_ids = [aws_subnet.eks_subnet_1.id, aws_subnet.eks_subnet_2.id]
